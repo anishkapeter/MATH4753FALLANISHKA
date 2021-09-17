d4b5af3ef57001f2f5eebddd838f13e828ee425c
Anishka Peter <anishka.a.peter-1@ou.edu>
2021-09-17 10:01:34 -0500

    Lab 4 function commit 1

---
+++ R/plotheight.R
@@ -0,0 +1,13 @@
+#' @title Quadratic Regression Function
+#'
+#' @param x A vector
+#'
+#' @return A vector
+#' @export
+#'
+#' @examples
+#' myplot(0)
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
+
